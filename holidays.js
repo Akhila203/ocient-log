f97a6da7932ea6ab1f5bf79fcc5f5dc32bbc5155
Akhila203 <128767609+Akhila203@users.noreply.github.com>
2026-05-02 09:04:26 +0530

    Add files via upload

---
+++ holidays.js
@@ -0,0 +1,33 @@
+// holidays.js
+const OFFICIAL_HOLIDAYS = {
+    // 2024 Holidays
+    "2024-01-01": "New Year's Day",
+    "2024-01-26": "Republic Day",
+    "2024-03-25": "Holi",
+    "2024-08-15": "Independence Day",
+    "2024-10-02": "Gandhi Jayanti",
+    "2024-10-31": "Diwali",
+    "2024-12-25": "Christmas Day",
+
+    // 2025 Holidays
+    "2025-01-01": "New Year's Day",
+    "2025-08-15": "Independence Day",
+    "2025-10-02": "Gandhi Jayanti",
+    "2025-10-20": "Diwali",
+    "2025-12-25": "Christmas Day",
+    
+    // 2026 Holidays
+    "2026-01-01": "New Year's Day",
+    "2026-01-26": "Republic Day",
+    "2026-03-04": "Holi",
+    "2026-05-01": "May day",
+    "2026-05-27": "Bakrid",
+    "2026-08-14": "Independence Day",
+    "2026-09-14": "Ganesh Chathurdhi",
+    "2026-10-02": "Gandhi Jayanthi",
+    "2026-10-20": "Dusshrea",
+    "2026-10-21": "Dusshrea",
+    "2026-11-02": "Kannada Rajotssava",
+    "2026-11-09": "Deepavali",
+    "2026-12-25": "Christmas"
+};
